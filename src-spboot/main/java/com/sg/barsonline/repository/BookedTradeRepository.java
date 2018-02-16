6197bc1f7ba34c56f50d9eb2cc1c4cd50d25c2d3
manazir <manazir.java@gmail.com>
2018-02-16 08:58:37 +0530

    spb

---
+++ src-spboot/main/java/com/sg/barsonline/repository/BookedTradeRepository.java
@@ -0,0 +1,14 @@
+package com.sg.barsonline.repository;
+
+import org.springframework.stereotype.Repository;
+
+//import org.springframework.data.repository.CrudRepository;
+
+//import com.sg.barsonline.entites.BarsReportEntity;
+
+@Repository
+public interface BookedTradeRepository  {//extends CrudRepository<BarsReportEntity, Long>{
+	
+	
+
+}
---
+++ src-spboot/main/java/com/sg/barsonline/repository/BrokerTradeRepository.java
@@ -0,0 +1,5 @@
+package com.sg.barsonline.repository;
+
+public interface BrokerTradeRepository {
+
+}
